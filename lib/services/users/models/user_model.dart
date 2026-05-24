4a0c0ee5cb78803250f2d910db2f66ff98f23fc5
jeffryendrianawijaya <95774894+jeflijonathan@users.noreply.github.com>
2026-05-24 18:13:09 +0700

    Remove redundant comments from user code
    Remove unnecessary inline comments in UserModel (toMap and fromMap) and strip a trailing inline comment in UsersService where null is returned. This is a non-functional cleanup to reduce clutter and keep code concise; no behavior changes.

--- lib/services/users/models/user_model.dart
+++ lib/services/users/models/user_model.dart
@@ -15,7 +15,6 @@ class UserModel {
     this.profilePictureUrl,
   });
 
-  // Convert user info to a Map for saving to Firestore
   Map<String, dynamic> toMap() {
     return {
       'uid': uid,
@@ -27,7 +26,6 @@ class UserModel {
     };
   }
 
-  // Create UserModel from a Firestore Map
   factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
       uid: map['uid'] ?? '',
